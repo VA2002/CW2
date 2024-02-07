9fba4e0a5cf57b0d0cb02921b96666dfda5e7d91
VA2002 <122964882+VA2002@users.noreply.github.com>
2024-02-07 18:34:58 +0400

    Update server.js

--- server.js
+++ server.js
@@ -231,7 +231,7 @@ app.get("/collection/:collectionName/search/:searchTerm", (req, res, next) => {
         res.status(500).json({ error: "Internal server error during search." });
       } else {
         res.json(results);
-        console.log(results);
+        console.log("Search results are ", results);
       }
     });
 });
