c633501b30e407bae2858fd427973af99ad9b8cd
VA2002 <122964882+VA2002@users.noreply.github.com>
2024-02-08 17:21:03 +0400

    Update server.js

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'index.html'));
+// });
 
 let db;
 
